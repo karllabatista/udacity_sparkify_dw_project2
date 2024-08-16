40e32abb003abb41816952f793f8a6c33b021120
karllabatistaa <kblb@icomp.ufam.edu.br>
2024-08-16 15:38:06 -0400

    add handle exception

--- create_tables.py
+++ create_tables.py
@@ -1,34 +1,60 @@
 import configparser
 import psycopg2
-from sql_queries import create_table_queries, drop_table_queries
 
+from sql_queries import create_table_queries,drop_table_queries
 
 def drop_tables(cur, conn):
-    for query in drop_table_queries:
-        cur.execute(query)
-        conn.commit()
+    status = False
+    try:
+        for query in drop_table_queries:
+            cur.execute(query)
+            conn.commit()
+        print("Tables dropped successfully")
+        status = True
+
+    except psycopg2.Error as error:
+        print("An unexpected error in Database occurred:",error) 
+
+    except Exception as error:   
+        print("An unexpected error occurred:",error) 
+
+    return status
 
 
 def create_tables(cur, conn):
-    for query in create_table_queries:
-        cur.execute(query)
-        conn.commit()
+    try:
+        for query in create_table_queries:
+            cur.execute(query)
+            conn.commit()
+
+        print("Tables created successfully")
+    
+    except psycopg2.Error as error:
+        print("An unexpected error in Database occurred:",error) 
+
+    except Exception as error:   
+        print("An unexpected error occurred:",error) 
 
 
 def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
-    print(conn)
-    cur = conn.cursor()
-
-    #drop_tables(cur, conn)
-    create_tables(cur, conn)
-
-    conn.close()
-
-
+    try:
+        conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+        print(conn)
+        cur = conn.cursor()
+        
+        if (drop_tables(cur, conn)):
+            create_tables(cur, conn)
+        else:
+            create_tables(cur, conn)
+        
+        conn.close()
+        cur.close()
+	    
+    except (psycopg2.DatabaseError, Exception) as error:
+        print("Connection failed!! Error:", error)
+        
 if __name__ == "__main__":
     main()

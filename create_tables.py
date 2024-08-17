1b52d05caf499a9f5fcd9e99d3f3e8dfb7daa4cc
karllabatistaa <kblb@icomp.ufam.edu.br>
2024-08-17 11:23:09 -0400

    add log message and rename main method

--- create_tables.py
+++ create_tables.py
@@ -6,6 +6,7 @@ from sql_queries import create_table_queries,drop_table_queries
 def drop_tables(cur, conn):
     status = False
     try:
+        print("Dropping tables ..")
         for query in drop_table_queries:
             cur.execute(query)
             conn.commit()
@@ -14,15 +15,18 @@ def drop_tables(cur, conn):
 
     except psycopg2.Error as error:
         print("An unexpected error in Database occurred:",error) 
+        status = False
 
     except Exception as error:   
         print("An unexpected error occurred:",error) 
+        status =False
 
     return status
 
 
 def create_tables(cur, conn):
     try:
+        print("Creating tables ..")
         for query in create_table_queries:
             cur.execute(query)
             conn.commit()
@@ -35,7 +39,7 @@ def create_tables(cur, conn):
     except Exception as error:   
         print("An unexpected error occurred:",error) 
 
-
+'''
 def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
@@ -55,6 +59,28 @@ def main():
 	    
     except (psycopg2.DatabaseError, Exception) as error:
         print("Connection failed!! Error:", error)
+'''        
+
+def inicialize_database():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    try:
+        conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+        print(conn)
+        cur = conn.cursor()
         
+        #if (drop_tables(cur, conn)):
+        #   create_tables(cur, conn)
+        #else:
+        drop_tables(cur, conn)
+        create_tables(cur, conn)
+        
+        conn.close()
+        cur.close()
+	    
+    except (psycopg2.DatabaseError, Exception) as error:
+        print("Connection failed!! Error:", error)
 if __name__ == "__main__":
-    main()
+    #main()
+    inicialize_database()

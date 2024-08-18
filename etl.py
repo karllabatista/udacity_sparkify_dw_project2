5f10651a8ddee62d093186c3cc41cef3ba8b3717
karllabatistaa <kblb@icomp.ufam.edu.br>
2024-08-18 18:43:37 -0400

    document etl functions

--- etl.py
+++ etl.py
@@ -4,6 +4,23 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """Load data from S3 to stages tables on redshift
+    
+    Parameters
+    ---------
+
+    cur:psycopg2.extensions.connection
+        Manager the connection with Redshift 
+
+    conn:sycopg2.extensions.cursor
+        Execute SQL commands and retrieve resulst from database
+
+    Returns:
+    -------
+    None
+    
+    """
+
     print("Loading stagging tables ..")
     try:
         
@@ -17,16 +34,43 @@ def load_staging_tables(cur, conn):
 
 
 def insert_tables(cur, conn):
+    """ Insert transformed data from stages tables to final tables
+
+    Parameters
+    ---------
+
+    cur:psycopg2.extensions.connection
+        Manager the connection with Redshift 
+
+    conn:sycopg2.extensions.cursor
+        Execute SQL commands and retrieve resulst from database
+
+    Returns:
+    -------
+    None
+    
+    """
     print("Inserting data ...")
     try:
         for query in insert_table_queries:
             cur.execute(query)
             conn.commit()
-        print("Data inserting completed successfully!!!")
+        print("Data inserted completed successfully!!!")
     except Exception as error:
         print(f"An error occurred:{error}")
 
 def main():
+    """Manage ETL data pipeline 
+    
+    Parameters
+    ---------
+    None
+
+    Returns:
+    -------
+    None
+    
+    """
     
     config = configparser.ConfigParser()
     config.read('dwh.cfg')

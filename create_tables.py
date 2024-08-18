9786bde5ff06a27ae9d1906d5ef3bea765bb54ab
karllabatistaa <kblb@icomp.ufam.edu.br>
2024-08-18 14:12:46 -0400

    improve functions

--- create_tables.py
+++ create_tables.py
@@ -4,24 +4,21 @@ import psycopg2
 from sql_queries import create_table_queries,drop_table_queries
 
 def drop_tables(cur, conn):
-    status = False
+    
     try:
         print("Dropping tables ..")
         for query in drop_table_queries:
             cur.execute(query)
             conn.commit()
         print("Tables dropped successfully")
-        status = True
-
+  
     except psycopg2.Error as error:
-        print("An unexpected error in Database occurred:",error) 
-        status = False
-
+        print("An unexpected error in Database occurred:",error)
+         
     except Exception as error:   
         print("An unexpected error occurred:",error) 
-        status =False
-
-    return status
+        
+   
 
 
 def create_tables(cur, conn):
@@ -64,23 +61,24 @@ def main():
 def inicialize_database():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
-
+   
+    
     try:
         conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
         print(conn)
         cur = conn.cursor()
         
-        #if (drop_tables(cur, conn)):
-        #   create_tables(cur, conn)
-        #else:
         drop_tables(cur, conn)
         create_tables(cur, conn)
-        
-        conn.close()
-        cur.close()
+    
 	    
     except (psycopg2.DatabaseError, Exception) as error:
         print("Connection failed!! Error:", error)
+    
+    finally:
+        conn.close()
+        cur.close()
+
 if __name__ == "__main__":
     #main()
     inicialize_database()

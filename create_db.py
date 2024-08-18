fc1e7df1a3520dd4097ca99d37e0df595f4f73e3
karllabatistaa <kblb@icomp.ufam.edu.br>
2024-08-18 13:33:00 -0400

    new class to create a databse

---
+++ create_db.py
@@ -0,0 +1,57 @@
+import boto3
+import json
+import configparser
+from cluster import Cluster             
+from create_tables import inicialize_database
+from etl import etl
+
+
+class CreateDatabase:
+
+    def __init__(self):
+        
+        config = configparser.ConfigParser()
+        config.read_file(open('dwh.cfg'))
+        self.KEY     = config.get('AWS','KEY')
+        self.SECRET  = config.get('AWS','SECRET')
+    
+    def run(self):
+        
+        ec2 = boto3.resource('ec2',
+                        region_name='us-west-2', 
+                        aws_access_key_id=self.KEY,
+                        aws_secret_access_key=self.SECRET)
+
+        s3 = boto3.client('s3',region_name='us-west-2', 
+                            aws_access_key_id=self.KEY,
+                            aws_secret_access_key=self.SECRET)
+
+        iam = boto3.client('iam',region_name='us-west-2', 
+                            aws_access_key_id=self.KEY,
+                            aws_secret_access_key=self.SECRET)
+
+        redshift = boto3.client('redshift',
+                                region_name='us-west-2', 
+                                aws_access_key_id=self.KEY,
+                                aws_secret_access_key=self.SECRET)
+        
+        cluster = Cluster()
+        ready = cluster.deploy_cluster(redshift,iam,ec2)
+        
+        if ready:
+
+            inicialize_database()
+           
+
+        #delete cluster function
+
+          
+       
+if __name__=='__main__':
+        
+    #print("que saco fazer isso")
+    create_db =CreateDatabase()
+    create_db.run()
+ 
+    
+            

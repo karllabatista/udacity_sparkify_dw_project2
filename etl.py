3957048ca4e834d66a984d85a09e6b238541c77a
karllabatistaa <kblb@icomp.ufam.edu.br>
2024-08-20 16:57:31 -0400

    uncomment function

--- etl.py
+++ etl.py
@@ -85,7 +85,7 @@ def main():
         conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
         cur = conn.cursor()
         
-        #load_staging_tables(cur, conn)
+        load_staging_tables(cur, conn)
         insert_tables(cur, conn)
 
     except Exception as error:

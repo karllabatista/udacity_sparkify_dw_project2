b275d4f79a72eda35285c787cc264f89bd10bb99
karllabatistaa <kblb@icomp.ufam.edu.br>
2024-08-20 16:53:02 -0400

    fix in insert songplays table

--- sql_queries.py
+++ sql_queries.py
@@ -171,17 +171,17 @@ staging_songs_copy = ("""
 songplay_table_insert = ("""
                          
 INSERT INTO SONGPLAYS(start_time,user_id,level,song_id,artist_id,session_id,location,user_agent)
-SELECT TIMESTAMP 'epoch' + se.ts / 1000 * INTERVAL '1 second' AS start_time,
+SELECT DISTINCT TIMESTAMP 'epoch' + se.ts / 1000 * INTERVAL '1 second' AS start_time,
         se.userID AS user_id,
         se.level,
-        se.song AS song_id,
-        se.artist AS artist_id,
+        ss.song_id AS song_id,
+        ss.artist_id AS artist_id,
         se.sessionId as session_id,
         se.location AS location,
         se.userAgent AS user_agent
 FROM staging_events se
+JOIN staging_songs ss on ss.artist_name = se.artist
 WHERE se.page ='NextSong';
-
 
 """)
 
